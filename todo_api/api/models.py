29d68006a0707acaff6facb39cad8546d9647388
takuo_ishida <archersparadox.100@gmail.com>
2021-04-24 13:32:30 +0900

    タイトルとコンテントに上限を追加

--- todo_api/api/models.py
+++ todo_api/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class TodoModel(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=400)
     delete_flg = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
