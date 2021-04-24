de9ff436acd95c973894ce14c648176f2cc09c25
takuo_ishida <archersparadox.100@gmail.com>
2021-04-24 13:33:47 +0900

    nullチェックを追加

--- todo_api/api/models.py
+++ todo_api/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class TodoModel(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField(max_length=400)
+    title = models.CharField(max_length=50, null=False)
+    content = models.TextField(max_length=400, null=False)
     delete_flg = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
