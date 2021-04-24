8ff0669b61dd18433b1969a2aa4b86b0d548a826
takuo_ishida <archersparadox.100@gmail.com>
2021-04-24 11:49:54 +0900

    urls.pyのTODO追加

--- todo_api/v1/api/urls.py
+++ todo_api/v1/api/urls.py
@@ -1,8 +1,9 @@
-from django import urls
-from django.urls import include, path
+from django.urls import path
 
 from .views import TodoViewSet
 
+# TODO: テスト工藤開発時に以下のURLパターンにして、各メソッドごとにテストをする
+# https://qiita.com/murapon/items/8a6a1715712ddd1a4ac1
 urlpatterns = [
     path('todos', TodoViewSet.as_view(), name="todo")
 ]
