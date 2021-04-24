5b15b44f67377815067a3a12b22d278196c0c47e
takuo_ishida <archersparadox.100@gmail.com>
2021-04-24 13:31:03 +0900

    削除フラグ追加・Filter追加

--- todo_api/api/views.py
+++ todo_api/api/views.py
@@ -1,10 +1,22 @@
 import json
 
+from django.db import models
+from django_filters import fields, filters
 from rest_framework import response, status, viewsets
 from rest_framework.views import APIView
 
 from .models import TodoModel
 from .serializers import TodoSerializer
+
+
+class TodoListFilter(filters.Filter):
+    title = filters.CharFilter(lookup_expr='icontains')
+    content = filters.CharFilter(lookup_expr='icontains')
+    delete_flg = filters.BooleanFilter(lookup_expr=False)
+
+    class Meta:
+        model = TodoModel
+        fields = '__all__'
 
 
 class TodoViewSet(APIView):
